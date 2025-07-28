0d85c32a9d239f4cbad55042f2b535e35f4a434c
Aman Bajoria <114388533+Aman296940@users.noreply.github.com>
2025-07-28 21:02:29 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: './', // ✅ necessary for Vercel/static builds
   plugins: [react()],
-  server: { proxy: { '/api': 'http://localhost:5000' } }
+  server: {
+    proxy: {
+      '/api': 'http://localhost:5000'
+    }
+  }
 });
